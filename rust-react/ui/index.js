97ce15784da664d85c6e010a960a5915c88ec4ab
arowe92 <32651627+arowe92@users.noreply.github.com>
2021-06-29 09:45:52 -0700

    Update index.js

--- rust-react/ui/index.js
+++ rust-react/ui/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h3>React BOOBIES</h3>, document.getElementById("root"));
+ReactDOM.render(<h3>React</h3>, document.getElementById("root"));
